475e86de4fffcb12de385549e80a2c8f9821fc0e
Omeiza Alabi <omeizaalabi@gmail.com>
2023-11-26 20:56:32 +0100

    Commenting

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,49 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+1-filter_states.py - Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa.
+
+Usage:
+  ./1-filter_states.py <mysql username> <mysql password> <database name>
+
+Description:
+  This script connects to a MySQL database and retrieves records from the 'states' table where
+  the state name starts with the letter 'N'. It is designed to be executed from the command line
+  with three required arguments:
+  - <mysql username>: The username to connect to the MySQL database.
+  - <mysql password>: The password associated with the provided username.
+  - <database name>: The name of the MySQL database containing the 'states' table.
+
+Requirements:
+  - Python 3
+  - MySQLdb module
+
+Example:
+  $ ./1-filter_states.py root password hbtn_0e_0_usa
+
+Note:
+  Make sure to replace 'root', 'password', and 'hbtn_0e_0_usa' with your MySQL username,
+  password, and the name of the database, respectively.
+
+Author:
+  [Your Name]
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Connect to the MySQL database using command-line arguments
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object to interact with the database
     c = db.cursor()
+
+    # Execute a MySQL query to select all records from the 'states' table ordered by 'id'
     c.execute("SELECT * FROM `states` ORDER BY `id`")
+
+    # Print states with names starting with 'N'
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""
+2-my_filter_states.py - Displays all values in the 'states' table of the database hbtn_0e_0_usa
+whose name matches that supplied as an argument.
+
+Usage:
+  ./2-my_filter_states.py <mysql username> <mysql password> <database name> <state name searched>
+
+Description:
+  This script connects to a MySQL database and retrieves records from the 'states' table where
+the state name matches the provided argument. It is designed to be executed from the command line
+with four required arguments:
+  - <mysql username>: The username to connect to the MySQL database.
+  - <mysql password>: The password associated with the provided username.
+  - <database name>: The name of the MySQL database containing the 'states' table.
+  - <state name searched>: The state name to search for in the 'states' table.
+"""
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    # Connect to the MySQL database using command-line arguments
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor object to interact with the database
     c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+
+    # Execute a MySQL query to select records from the 'states' table
+    # where the state name matches the provided argument
+    c.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+
+    # Print the retrieved states
     [print(state) for state in c.fetchall()]
